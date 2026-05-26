025719b198513fec15718c977fba5be829afbaa1
prof-salu <saluorodrigues@gmail.com>
2026-05-26 16:30:52 -0300

    estilos

--- App.js
+++ App.js
@@ -4,15 +4,27 @@ import { useState } from 'react';
 export default function App() {
   const [item, setItem] = useState('');
   const [quantidade, setQuantidade] = useState('');
+  const [lista, setLista] = useState([{id : 1, nome : 'mouse', quantidade : 12}]);
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Item' 
+      <TextInput placeholder='Item' style={styles.input}
                  onChangeText={setItem} value={item}/>
 
-      <TextInput placeholder='Quantidade' 
-                 onChangeText={setQuantidade}/>
+      <TextInput placeholder='Quantidade' style={styles.input}
+                 onChangeText={setQuantidade} value={quantidade}/>
+
       <Button title='Gravar' />
+
+      <FlatList 
+        data={lista}
+        keyExtractor={id => item.id}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.texto}>{item.nome} - {item.quantidade}</Text>
+            <Button title='Apagar' color={'red'}/>
+          </View>
+  )} />
     </View>
   );
 }
@@ -20,5 +32,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
-  },
+  }, item: {
+    flexDirection: 'row', padding: 10, alignItems: 'center'
+  }, texto: { 
+    fontSize: 16, marginRight: 10, 
+  }, input: {
+    borderWidth: 1, padding: 5, marginBottom: 10,
+  }
 });
